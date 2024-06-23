7f9d8c14a3decaef03785d7865e2e6ce2429a90f
Danish <danishali0051@gmail.com>
2024-06-23 23:23:52 +0500

    added: aussie no thanks setuped

--- App.tsx
+++ App.tsx
@@ -1,118 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, Text} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+interface Company {
+  name: string;
+  priorityTarget: string;
+  type: string;
+  rationale: string;
+  alternateWay: string;
+  link: string;
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const sheetId = '19JIdSn8QT9mTwMWWKC8uJyF2auMeNEER1lexJCkgyrY';
+const apiKey = 'AIzaSyBCQOCjYVY9GFJxSHz2gbyXapKPciPs44M';
+const sheetName = 'Companies';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const transformCompaniesData = data => {
+  const transformedData: Company[] = [];
+
+  data.forEach(item => {
+    const company: Company = {} as Company;
+
+    company.name = item[0];
+    company.priorityTarget = item[1];
+    company.type = item[2];
+    company.rationale = item[3];
+    company.alternateWay = item[4];
+    company.link = item[5];
+
+    transformedData.push(company);
+  });
+
+  return transformedData;
+};
+
+function App(): React.JSX.Element {
+  const [companiesData, setCompaniesData] = useState<Company[]>([]);
+
+  useEffect(() => {
+    fetch(
+      `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}?key=${apiKey}`,
+    )
+      .then(response => response.json())
+      .then(data => {
+        data.values.shift();
+
+        const transformedData: Company[] = transformCompaniesData(data.values);
+        setCompaniesData(transformedData);
+      })
+      .catch(error => console.error('Error:', error));
+  }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <Text>NoThanks Aussie</Text>
+      <Text>Here are the companies we should say No Thanks to: </Text>
+      {companiesData.length > 0 ? (
+        companiesData.map(company => (
+          <Text key={company.name}>Company Name: {company.name}</Text>
+        ))
+      ) : (
+        <Text>Loading...</Text>
+      )}
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
